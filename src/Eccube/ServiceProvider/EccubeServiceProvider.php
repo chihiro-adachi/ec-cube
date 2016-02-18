2bcfb9c9b2f2e280f2b31b6f4a6ec1b85058ebbc
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2016-02-18 20:33:39 +0900

    項目削除のサンプル

--- src/Eccube/ServiceProvider/EccubeServiceProvider.php
+++ src/Eccube/ServiceProvider/EccubeServiceProvider.php
@@ -263,6 +263,7 @@ class EccubeServiceProvider implements ServiceProviderInterface
         $app['form.type.extensions'] = $app->share($app->extend('form.type.extensions', function ($extensions) use ($app) {
             $extensions[] = new \Eccube\Form\Extension\HelpTypeExtension();
             $extensions[] = new \Eccube\Form\Extension\FreezeTypeExtension();
+            $extensions[] = new \Eccube\Form\Extension\EntryTypeExtension();
 
             return $extensions;
         }));
