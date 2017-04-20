657effeb0c1f0dbeee4a7f695337e80e63e861ad
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2017-04-20 10:59:02 +0900

    4. コントローラを作る：カテゴリのデータを取得する

--- src/Eccube/Controller/Block/CategoryFooterController.php
+++ src/Eccube/Controller/Block/CategoryFooterController.php
@@ -6,6 +6,8 @@ class CategoryFooterController
 {
     public function index(\Eccube\Application $app)
     {
+        $Categories = $app['eccube.repository.category']->getList();
+
         return $app->render('Block/category_footer.twig');
     }
 }
