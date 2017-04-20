0fe5529cb3a34f8b49d1d7bdf83c3addbbf5016a
chihiro-adachi <chihiro_adachi@lockon.co.jp>
2017-04-20 10:56:01 +0900

    4. コントローラを作る：まずは文字列をだしてみる

---
+++ src/Eccube/Controller/Block/CategoryFooterController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Eccube\Controller\Block;
+
+class CategoryFooterController
+{
+    public function index(\Eccube\Application $app)
+    {
+        return "コントローラを経由してカテゴリを表示する";
+    }
+}
