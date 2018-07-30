317bf9e9f0b8e48f4aae42d30c520b6242fdf927
Chihiro Adachi <8196725+chihiro-adachi@users.noreply.github.com>
2018-07-30 09:28:07 +0900

    traitのサンプルを追加

---
+++ app/Plugin/Sample3/Entity/CustomerTrait.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of EC-CUBE
+ *
+ * Copyright(c) LOCKON CO.,LTD. All Rights Reserved.
+ *
+ * http://www.lockon.co.jp/
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Plugin\Sample3\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Eccube\Annotation\EntityExtension;
+
+/**
+ * @EntityExtension("Eccube\Entity\Customer")
+ */
+trait CustomerTrait
+{
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sample3", type="string", length=255)
+     */
+    private $sample3;
+}
